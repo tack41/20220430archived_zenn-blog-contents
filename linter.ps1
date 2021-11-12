8ffc627935631beb4af191715dd700ed9ebfe292
Takashi Tsuduki <tack41@gmail.com>
2021-11-13 06:33:54 +0900

    20211113

--- linter.ps1
+++ linter.ps1
@@ -1,1 +1,7 @@
-npx markdownlint .
+Set-StrictMode -Version 3.0
+$ErrorActionPreference = ÅgStopÅh
+$CURRENT_DIR = $PSScriptRoot
+
+Push-Location $CURRENT_DIR
+npx markdownlint . --ignore node_modules/
+Pop-Location
