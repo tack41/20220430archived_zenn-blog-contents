368e29e0c8a19f77f02479ed0eda4697a7775c5c
Takashi Tsuduki <tack41@gmail.com>
2021-11-13 06:58:20 +0900

    20211113-2

--- linter.sh
+++ linter.sh
@@ -1,1 +1,9 @@
+SCRIPT_NAME=$(basename "$0")
+CURRENT_DIR_RELATIVE=$(dirname "$0")
+CURRENT_DIR=$(cd "$CURRENT_DIR_RELATIVE" || exit;pwd)
+PARENT_DIR=$(cd "$CURRENT_DIR"/.. || exit;pwd)
+#GRANDPARENT_DIR=$(cd "$PARENT_DIR"/.. || exit;pwd)
+
+pushd "$CURRENT_DIR"
 npx markdownlint . --ignore ./node_modules
+popd
